9c7d37170504bfb7b9ef2103407f13f49f355696
depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2024-11-13 23:36:28 +0530

    Removing incomplete command

--- cogs/animanga/__init__.py
+++ cogs/animanga/__init__.py
@@ -2,14 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from .anime import Anime
 from .waifu import Waifu
 
 if TYPE_CHECKING:
     from bot import DeBot
 
 
-class AniManga(Waifu, Anime, name='Anime & Manga'): ...
+class AniManga(Waifu, name='Anime & Manga'): ...
 
 
 async def setup(bot: DeBot) -> None:
