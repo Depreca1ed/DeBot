72ffb64c4a11d8386b3d73aa2ab7fce49cd6617b
Deprecatism <70801324+Deprecatism@users.noreply.github.com>
2026-09-11 17:35:43 +0530

    Chanfe fixupx to fixxv

--- extensions/utility/__init__.py
+++ extensions/utility/__init__.py
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
     from utilities.bases.context import CyContext
 
 FIXUP_REPLACE = {
-    r'https?:\/\/(?:x|twitter|cunnyx)\.com\/(?:\w+)\/status\/(\d+)': r'https://fixupx.com/status/\g<1>',
+    r'https?:\/\/(?:x|twitter|cunnyx)\.com\/(?:\w+)\/status\/(\d+)': r'https://fixvx.com/status/\g<1>',
     r'https?:\/\/open\.(?:spotify)\.com\/track\/([a-zA-Z0-9]+)': r'https://fxspotify.com/track/\g<1>',
     r'https?:\/\/(?:www\.)?(?:instagram)\.com\/(?:(?!(?:p|reels?)\/)\S+\/)?(p|reels?)\/(\S+)(?:\??\S+)?': r'https://oginstagram.com/\g<1>/\g<2>',
 }
